0768d17506dc579a23161cd36cdc747e6586980f
iammancy <iammancy@gmail.com>
2017-12-14 16:49:29 +0530

    Added Header and Components

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { routes } from './app.router';
 
 import { AppComponent } from './app.component';
+import { AppTemplateDriven } from "./Template Driven/app.template-driven";
+import { ModelDrivenComponent } from './model-driven/model-driven.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppTemplateDriven,
+    ModelDrivenComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
